091be79e911add67797a2a4e3af86540b3e461ed
Abhishek Anand <abhi@solv.tv>
2012-11-04 23:17:33 +0530

    inital template integration

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nammateacher::Application.config.secret_token = '47c739d1cba075f40e6b0e78462750747f5db549bfdbe21f844202ba6dd4e5ca14976761b262b20854bc1f6a6d3df1e76078aa833ee2dfefb8df284e6946644f'
