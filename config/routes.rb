d0a4b277ea6d1a27857147ca48205b77b38dcbc4
Abhishek Anand <abhi@solv.tv>
2012-11-05 00:17:01 +0530

    under construction page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Nammateacher::Application.routes.draw do
-  root to: "application#index"
+  root to: "application#under_construction"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
